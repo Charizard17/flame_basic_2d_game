9de47c8c5756a77081bc2b2051d73651816709ab
Esad Dursun <3723esad@gmail.com>
2022-01-14 11:57:37 +0100

    set up flame game loop and create player

--- lib/main_game_page.dart
+++ lib/main_game_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'helpers/direction.dart';
-import 'helpers/joypad.dart';
+import 'package:flame/game.dart';
+
+import './helpers/direction.dart';
+import './helpers/joypad.dart';
+import './ray_world.game.dart';
 
 class MainGamePage extends StatefulWidget {
   const MainGamePage({Key? key}) : super(key: key);
@@ -10,13 +13,15 @@ class MainGamePage extends StatefulWidget {
 }
 
 class MainGameState extends State<MainGamePage> {
+  RayWorldGame game = RayWorldGame();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
         body: Stack(
           children: [
-            // TODO 1
+            GameWidget(game: game),
             Align(
               alignment: Alignment.bottomRight,
               child: Padding(
