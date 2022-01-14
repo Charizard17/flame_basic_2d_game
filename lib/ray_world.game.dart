7f34e0a5cc2022359e01ce08ac0d5876234a21ef
Esad Dursun <3723esad@gmail.com>
2022-01-14 21:54:20 +0100

    keyboard inputs added

--- lib/ray_world.game.dart
+++ lib/ray_world.game.dart
@@ -2,6 +2,9 @@ import 'dart:ui';
 
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './components/world_collidable.dart';
 import './components/world.dart';
@@ -9,7 +12,7 @@ import './components/player.dart';
 import './helpers/direction.dart';
 import './helpers/map_loader.dart';
 
-class RayWorldGame extends FlameGame with HasCollidables {
+class RayWorldGame extends FlameGame with HasCollidables, KeyboardEvents {
   final Player _player = Player();
   final World _world = World();
 
@@ -32,10 +35,37 @@ class RayWorldGame extends FlameGame with HasCollidables {
   }
 
   void addWorldCollision() async =>
-      (await MapLoader.readRayWorldCollisionMap()).forEach((rect) {
-        add(WorldCollidable()
-          ..position = Vector2(rect.left, rect.top)
-          ..width = rect.width
-          ..height = rect.height);
-      });
+      (await MapLoader.readRayWorldCollisionMap()).forEach(
+        (rect) {
+          add(WorldCollidable()
+            ..position = Vector2(rect.left, rect.top)
+            ..width = rect.width
+            ..height = rect.height);
+        },
+      );
+
+  @override
+  KeyEventResult onKeyEvent(
+      RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isKeyDown = event is RawKeyDownEvent;
+    Direction? keyDirection = null;
+
+    if (event.logicalKey == LogicalKeyboardKey.keyA) {
+      keyDirection = Direction.left;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyD) {
+      keyDirection = Direction.right;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyW) {
+      keyDirection = Direction.up;
+    } else if (event.logicalKey == LogicalKeyboardKey.keyS) {
+      keyDirection = Direction.down;
+    }
+
+    if (isKeyDown && keyDirection != null) {
+      _player.direction = keyDirection;
+    } else if (_player.direction == keyDirection) {
+      _player.direction = Direction.none;
+    }
+
+    return super.onKeyEvent(event, keysPressed);
+  }
 }
