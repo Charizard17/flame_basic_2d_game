aeeffb0c8bb0b7f07216e98798fbf3dc9a3fa385
Esad Dursun <3723esad@gmail.com>
2022-01-14 15:02:52 +0100

    world added

--- lib/ray_world.game.dart
+++ lib/ray_world.game.dart
@@ -1,14 +1,26 @@
+import 'dart:ui';
+
 import 'package:flame/game.dart';
 
 import './components/player.dart';
+import './components/world.dart';
 import './helpers/direction.dart';
 
 class RayWorldGame extends FlameGame {
   final Player _player = Player();
+  final World _world = World();
 
   @override
   Future<void> onLoad() async {
+    super.onLoad();
+    await add(_world);
     add(_player);
+
+    _player.position = _world.size / 2;
+    camera.followComponent(
+      _player,
+      worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y),
+    );
   }
 
   void onJoypadDirectionChanged(Direction direction) {
